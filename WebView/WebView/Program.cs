f587eff7fa2526b763485f417f4e262669b2d7b2
Rohit Jha <rohit305jha@gmail.com>
2016-11-22 22:50:11 -0800

    Updated code to include EyeXFramework reference (copied from sample winforms project)

--- WebView/WebView/Program.cs
+++ WebView/WebView/Program.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EyeXFramework.Forms;
 
 namespace WebView
 {
     static class Program
     {
+        private static FormsEyeXHost _eyeXHost = new FormsEyeXHost();
+
+        /// <summary>
+        /// Gets the singleton EyeX host instance.
+        /// </summary>
+        public static FormsEyeXHost EyeXHost
+        {
+            get { return _eyeXHost; }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            _eyeXHost.Start();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            _eyeXHost.Dispose();
         }
     }
 }
