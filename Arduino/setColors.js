fc3cd5965c751967e96fed4145b92709feabe71a
Kronebusch, Robert A <RAKronebusch@directv.com>
2016-12-01 12:31:12 -0800

    added setColors function

---
+++ Arduino/setColors.js
@@ -0,0 +1,22 @@
+function setColors(reading) {
+	if(reading < 150) {
+		document.body.style.color="black";
+		document.body.style.background = "white";
+	}
+	else if(reading < 325) {
+		document.body.style.color="black";
+		document.body.style.background = "white";
+	}
+	else if(reading < 440) {
+		document.body.style.color="black";
+		document.body.style.background = "white";
+	}
+	else if(reading < 570) {
+		document.body.style.color="black";
+		document.body.style.background = "white";
+	}
+	else {
+		document.body.style.color="black";
+		document.body.style.background = "white";
+	}
+}
